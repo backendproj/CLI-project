93b88a89290d7422e237dd8ae3ea51d9989db7f5
Erkinbek008 <amiroverkinbek008@gmail.com>
2022-12-13 16:13:06 +0500

    file for CLI project

--- file.py
+++ file.py
@@ -1,1 +1,24 @@
-# Erkinbek va Madaminbek
+from pathlib import Path
+print("Assalomu alekum!")
+def papkaga_kirish(kirish):
+        print(Path.home() /"Desktop"/kirish)
+def papka_yaratish(papka):
+    try:
+            desk = Path.home() /"Desktop"/papka
+            desk.mkdir()
+    except FileExistsError:
+        print("Papka allaqachon yaratilgan!")
+def file_yaratish(file):
+    try:
+            ds = Path.home() / "Desktop"/file
+            ds.touch()
+    except FileExistsError:
+        print("File allaqachon yaratilgan!")
+def ozgartirish(old_papka,new_papka,file):
+    desktop = Path.home() / "Desktop"
+    new = desktop /old_papka /file
+    old = desktop / new_papka / file
+    new.replace(old)
+def ochirish(delate_papka):
+        delate = Path.home() / "Desktop" / delate_papka
+        delate.rmdir()
